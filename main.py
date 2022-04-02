e72dfacae2b00d396307a7ff13229a0274cd8403
Satria Rahmat <64813392+Nux-xader@users.noreply.github.com>
2022-04-03 05:31:33 +0800

    initial

---
+++ main.py
@@ -0,0 +1,70 @@
+from utils import *
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+APP_NAME = "Qur'an Online"
+DEVELOPENT = True
+
+
+@app.route("/")
+def home():
+	return render_template(
+		"pages/index.html", 
+		app_name=APP_NAME, 
+		page="Home"
+	)
+
+
+@app.route("/juz")
+def juz():
+	if "juz" in request.args:
+		juz = request.args.get('juz')
+		return render_template(
+			"pages/isi_juz.html", 
+			app_name=APP_NAME, 
+			page=f"Juz {juz}", 
+			juz=juz
+		)
+
+	else:
+		all_juz = list_divider(6, [i for i in range(1, 31)])
+		return render_template(
+			"pages/juz.html", 
+			app_name=APP_NAME, 
+			page="Juz", 
+			all_juz=all_juz
+		)
+
+
+@app.route("/surah")
+def surah():
+	if "surah" in request.args:
+		surah = request.args.get('surah')
+		return render_template(
+			"pages/isi_surah.html", 
+			app_name=APP_NAME, 
+			page=f"Surah {surah}", 
+			surah=surah
+		)
+
+	else:
+		return render_template(
+			"pages/surah.html", 
+			app_name=APP_NAME, 
+			page="Surah"
+		)
+
+@app.route("/cari")
+def cari():
+	return render_template(
+		"pages/cari.html", 
+		app_name=APP_NAME, 
+		page="Cari Potongan Ayat"
+	)
+
+
+if __name__ == '__main__':
+	if DEVELOPENT:
+		app.run(debug=True, host="0.0.0.0")
+	else:
+		app.run()
